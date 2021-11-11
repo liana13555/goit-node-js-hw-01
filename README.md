a6d8de9094e5926910caa44df1b6a75b7925b8de
Liana Baryshova <barishovaliana@gmail.com>
2021-11-11 23:25:56 +0100

    finished homework

---
+++ README.md
@@ -0,0 +1,23 @@
+# Получаем и выводим весь список контактов в виде таблицы (console.table)
+
+node index.js --action list
+
+https://ibb.co/y0cj3tF
+
+# Получаем контакт по id
+
+node index.js --action get --id 5
+
+https://ibb.co/yhD77rw
+
+# Добавялем контакт
+
+node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22
+
+https://ibb.co/rpJWb2Z
+
+# Удаляем контакт
+
+node index.js --action remove --id=3
+
+https://ibb.co/YDc3Rgj
---
+++ index.js
@@ -0,0 +1,44 @@
+const { listContacts, getContactById, removeContact, addContact } = require('./contacts');
+
+const { Command } = require('commander');
+const program = new Command();
+program
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+// TODO: рефакторить
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+      case 'list':
+          const contacts = await listContacts();
+          console.table(contacts);
+          break;
+
+    case 'get':
+          const contactById = await getContactById(id);
+          console.table(contactById);
+      break;
+
+    case 'add':
+          const newContact = await addContact(name, email, phone);
+          console.table(newContact);
+      break;
+
+    case 'remove':
+          const deletedContact = await removeContact(id);
+          console.table(deletedContact);
+      break;
+
+    default:
+      console.warn('\x1B[31m Unknown action type!');
+  }
+}
+
+invokeAction(argv);
